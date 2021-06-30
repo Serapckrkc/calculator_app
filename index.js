350414ec719f16b97ae26e773b62e08d42d5c47b
Serapckrkc <Serapckrkc@gmail.com>
2021-06-30 11:16:30 +0300

    Functions Added

---
+++ index.js
@@ -0,0 +1,10 @@
+const numbers = document.querySelectorAll(".contanier-key__row")
+const reset = document.getElementById("reset")
+const equal = document.getElementById("equal")
+const screen = document.getElementById("screen")
+
+console.log(numbers)
+console.log(reset)
+console.log(equal)
+console.log(screen)
+
