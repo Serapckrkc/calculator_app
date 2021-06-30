258f78ba0ee9a1ac93f9f7880c551f4370a350e5
Serapckrkc <Serapckrkc@gmail.com>
2021-06-30 13:04:45 +0300

    Functions Edit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const numbers = document.querySelectorAll(".contanier-key__row")
-const reset = document.getElementById("reset")
-const equal = document.getElementById("equal")
-const screen = document.getElementById("screen")
-
-console.log(numbers)
-console.log(reset)
-console.log(equal)
-console.log(screen)
+const screenCalc = document.getElementById("screen-calc")
+con
+function  handleClick(value){
+    screenCalc.text.value += value
+}
+ 
+function clear(){
+    screenCalc.text.value = ''
+}
 
