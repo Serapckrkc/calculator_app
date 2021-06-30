8c501c36dca794eac0242f23efd18fb4b7d56979
Serap Büyükçıkrıkçı <83032285+Serapckrkc@users.noreply.github.com>
2021-06-30 11:19:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-const numbers = document.querySelectorAll(".contanier-key__row")
-const reset = document.getElementById("reset")
-const equal = document.getElementById("equal")
-const screen = document.getElementById("screen")
-
-console.log(numbers)
-console.log(reset)
-console.log(equal)
-console.log(screen)
 
